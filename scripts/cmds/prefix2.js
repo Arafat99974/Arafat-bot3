5dbe0006038676de33f13811c5905b124251eb9c
Arafat99974 <arafatas602@gmail.com>
2025-04-05 19:23:18 +0600

    Update prefix2.js

--- scripts/cmds/prefix2.js
+++ scripts/cmds/prefix2.js
@@ -11,16 +11,16 @@ module.exports = {
  },
 
  onStart: async function(){}, 
- onChat: async function({ event, message, getLang }) {
+ onChat: async function({ event, message, getLang } ) {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
  body: `
-Yo, my prefix is [ 𓆩 ( 𓆪 ]\n
+Yo, my prefix is [ 𓆩 # 𓆪 ]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
-➥ 𝙊𝙒𝙉𝙀𝙍: ~𝗘𝘄𝗿 𝗦𝗵𝗔𝗻'𝘀
+➥ 𝙊𝙒𝙉𝙀𝙍: ~Arafat
 ➥ ✓sim [bby] -> talk to bot
 ➥ ✓callad [message] -> report any problem encountered
-➥ ✓help -> usage of command\n\nHave fun using it enjoy!❤️\nBot Developer:https://www.facebook.com/sirana252`,
+➥ ✓help -> usage of command\n\nHave fun using it enjoy!❤️\nBot Developer:https://www.facebook.com/Arafatas602`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/M4luPbE.gif")
  });
  }
